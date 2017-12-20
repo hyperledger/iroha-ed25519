56d926c4489aa7b2bb664a48294c5ee2d6e1c283
Bogdan Vaneev <warchantua@gmail.com>
2017-12-20 17:28:04 +0900

    Add example and fix mac compilation error
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

---
+++ example/main.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <ed25519/ed25519.h>
+
+int main(int arc, char** argv) {
+  public_key_t pub;
+  private_key_t priv;
+  int ret = ed25519_create_keypair(&priv, &pub);
+  assert(ret != 0);  // status code: ok
+
+  const int size           = 8;
+  unsigned char *buff = "ed25519";
+
+  signature_t sig;
+  ed25519_sign(&sig, buff, size, &pub, &priv);
+
+  ret = ed25519_verify(&sig, buff, size, &pub);
+  assert(ret != 0);  // correct result
+
+  return 0;
+}
